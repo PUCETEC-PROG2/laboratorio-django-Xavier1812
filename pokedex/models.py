36438d4cbd8febbbcc2405d8ff3d64313007f9b3
Bryan Xavier Paredes Loyola <xavier.paredesloyola@gmail.com>
2024-12-18 17:41:53 -0500

    lab8

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Pokemon(models.Model):
+    pass
+    #Django no necesita un innit
+    name = models.CharField(max_length=30, null=False) #Para VARCHAR
+    type = models.CharField(max_length=30, null=False)
+    weight = models.DecimalField(max_digits=6, decimal_places=4)
+    height = models.DecimalField(max_digits=6, decimal_places=4)
+    
+    def __str__(self):
+        return self.name
