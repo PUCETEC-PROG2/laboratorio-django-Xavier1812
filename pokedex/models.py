b37e35cffc39221b09ad5d7d4ce1aa0696c1a92c
Bryan Xavier Paredes Loyola <xavier.paredesloyola@gmail.com>
2025-01-05 20:52:39 -0500

    Cambios modelo Laboratorio 8

--- pokedex/models.py
+++ pokedex/models.py
@@ -13,7 +13,7 @@ class Pokemon(models.Model):
     name = models.CharField(max_length=30, null=False) #Para VARCHAR
     type = models.CharField(max_length=30, null=False)
     weight = models.DecimalField(max_digits=6, decimal_places=4)
-    height = models.DecimalField(max_digits=6, decimal_places=4)
+    height = models.DecimalField(max_digits=7, decimal_places=4)
     
     def __str__(self):
         return self.name
