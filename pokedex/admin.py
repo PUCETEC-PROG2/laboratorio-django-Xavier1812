6d83b2372d3f2bc51becc4871c77fafd00042596
Bryan Xavier Paredes Loyola <xavier.paredesloyola@gmail.com>
2025-01-05 19:01:47 -0500

    Nuevos cambios

--- pokedex/admin.py
+++ pokedex/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Pokemon
+from .models import Pokemon, Trainer
 
 # Register your models here.
 @admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    pass
 
-class POkemonAdmin(admin.ModelAdmin):
+@admin.register(Trainer)
+class TrainerAdmin(admin.ModelAdmin):
     pass
--- pokedex/models.py
+++ pokedex/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Trainer(models.Model):
+    first_name = models.CharField(max_length=30, null=False)
+    last_name = models.CharField(max_length=30, null=False)
+    birth_date = models.DateField()
+    level = models.IntegerField(default=1)
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
 class Pokemon(models.Model):
-    pass
-    #Django no necesita un innit
     name = models.CharField(max_length=30, null=False) #Para VARCHAR
     type = models.CharField(max_length=30, null=False)
     weight = models.DecimalField(max_digits=6, decimal_places=4)
@@ -11,3 +17,4 @@ class Pokemon(models.Model):
     
     def __str__(self):
         return self.name
+
